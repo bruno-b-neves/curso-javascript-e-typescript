d84ef3f66e8f9d479028ba9f2eb4541b33e8791b
Bruno Barbosa Neves <brunobningles@gmail.com>
2026-07-24 23:47:05 -0300

    aula 3 | Curso de JavaScript e TypeScript

---
+++ JavaScript Basico/aula03/index.js
@@ -0,0 +1,19 @@
+/* --- CONSOLE LOG --- */
+// todo | No console.log, podemos escrever textos (string), digitar números, 
+// todo | números flutuantes utilizam (.)
+
+// * * Textos podem ser escritos em "" (duplas) ou '' (simples)
+console.log('Bruno');
+console.log("Neves");
+
+// ! Não podemos colocar '' (simples) dentro de '' (simples) quando escrevemos textos
+// ! o mesmo acontece para "" (duplas).
+console.log("Bruno 'Neves'");
+console.log('Bruno "Neves"');
+
+// * Podemos utilizar `` (template string) para escrever textos e ainda podemos 
+// * utilzar '' (simples) ou "" (duplas) dentro de template string.
+console.log(`Bruno Neves`);
+
+
+console.log(12, 32.25, 'Bruno Neves');
